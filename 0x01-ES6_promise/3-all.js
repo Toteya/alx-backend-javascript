dea08eb49fdad47ce03c35ff0858633e52abef42
Toteya <111172685+Toteya@users.noreply.github.com>
2024-06-27 16:24:14 +0200

    handle all promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+
+  Promise.all([promise1, promise2]).then(
+    (value) => { console.log(value[0].body, value[1].firstName, value[1].lastName); },
+    () => { console.error('Signup system offline'); },
+  );
+}
