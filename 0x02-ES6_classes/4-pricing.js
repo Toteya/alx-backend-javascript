0ff8dec80019dafb4e4f99cf3fcc36c181a7e827
Toteya <111172685+Toteya@users.noreply.github.com>
2024-06-28 14:31:22 +0200

    Task 4: correction

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -32,6 +32,6 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} ${this._currency.displayFullCurrency()}`;
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
   }
 }
