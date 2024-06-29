b2e2f1954c125f791fd7d4d4e3eb13d086790431
Toteya <111172685+Toteya@users.noreply.github.com>
2024-06-29 13:07:25 +0200

    correction: wrong output

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arr = [];
   for (let idx of array) {
-    const value = array[idx];
+    const value = idx;
     idx = appendString + value;
     arr.push(idx);
   }
