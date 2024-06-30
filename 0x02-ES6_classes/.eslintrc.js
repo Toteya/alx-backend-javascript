1b85c1a0e3baa0f2fd9d2cad4dd6741d119d666b
Toteya <111172685+Toteya@users.noreply.github.com>
2024-06-30 09:29:33 +0200

    commit all untracked changes

--- 0x02-ES6_classes/.eslintrc.js
+++ 0x02-ES6_classes/.eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
 		    },
 	  plugins: ['jest'],
 	  rules: {
+        "no-underscore-dangle": 'off',
 		      'no-console': 'off',
 		      'no-shadow': 'off',
 		      'no-restricted-syntax': [
