8650ac20a7d57de6cf2004ed1fa0cc9259564c82
Toteya <111172685+Toteya@users.noreply.github.com>
2024-06-30 20:50:13 +0200

    Try fix: async await

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
+export default async function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
 
-  return Promise.all([promise1, promise2])
+  return await Promise.all([promise1, promise2])
     .then((value) => {
       console.log(value[0].body, value[1].firstName, value[1].lastName);
     })
