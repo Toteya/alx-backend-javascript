c495dd6762c55ba3f2b2ed8361081a5f47d02664
Toteya <111172685+Toteya@users.noreply.github.com>
2024-07-01 20:56:16 +0200

    Fixed output

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,14 +4,13 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
-  
+
   return Promise.allSettled([promise1, promise2])
     .then((result) => {
       const arr = [];
       for (const item of result) {
-        const status = item.status;
-        const value = item.value !== undefined? item.value : item.reason;
-        arr.push({status, value})
+        const value = item.value !== undefined ? item.value : `${item.reason}`;
+        arr.push({ status: item.status, value });
       }
       return arr;
     });
