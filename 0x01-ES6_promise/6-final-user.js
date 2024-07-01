4f4a45d1a6f8070e05aad6e6763cb1defc15e830
Toteya <111172685+Toteya@users.noreply.github.com>
2024-07-01 11:11:38 +0200

    Fixed: eslint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,10 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
-  
+
   return Promise.allSettled([promise1, promise2])
     .then((result) => result);
 }
