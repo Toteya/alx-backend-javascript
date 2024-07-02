8e2004d826f2dd2977b86355b38d0a6d847496d8
Toteya <111172685+Toteya@users.noreply.github.com>
2024-07-02 13:06:20 +0200

    Fixed: remove validation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,10 +1,6 @@
 export default function getStudentIdsSum(students) {
-  let studentIdsSum = 0;
-  if (!(students instanceof Array)) { return studentIdsSum; }
-
   const studentIds = students.map((student) => student.id);
-
-  studentIdsSum = studentIds.reduce(
+  const studentIdsSum = studentIds.reduce(
     (previousValue, currentValue) => previousValue + currentValue,
   );
 
