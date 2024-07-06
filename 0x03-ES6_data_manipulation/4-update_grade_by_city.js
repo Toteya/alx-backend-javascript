064e8461b462c53c40e88fa575765df67fa57a8e
Toteya <111172685+Toteya@users.noreply.github.com>
2024-07-06 18:02:17 +0200

    fixed: must use map

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,29 @@
-import getStudentsByLocation from './2-get_students_by_loc';
-
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const studentsByLoc = getStudentsByLocation(students, city);
-  const studentNewGrades = [];
+  let studentsNewGrades = [];
+  const studentsByLoc = students.filter((student) => student.location === city);
 
-  for (const student of studentsByLoc) {
-    try {
-      student.grade = newGrades.filter((record) => record.studentId === student.id)[0].grade;
-    } catch (TypeError) {
-      student.grade = 'N/A';
-    }
-    studentNewGrades.push(student);
+  studentsNewGrades = studentsByLoc.map((student) => new Object({
+    ...student,
+    grade: newGrades.filter((record) => record.studentId === student.id)[0] !== undefined
+      ? newGrades.filter((record) => record.studentId === student.id)[0].grade
+      : 'N/A',
+  }));
+
+  for (const student of studentsNewGrades) {
+
   }
 
-  return studentNewGrades;
+  // const studentsByLoc = getStudentsByLocation(students, city);
+  // const studentNewGrades = [];
+
+  // for (const student of studentsByLoc) {
+  //   try {
+  //     student.grade = newGrades.filter((record) => record.studentId === student.id)[0].grade;
+  //   } catch (TypeError) {
+  //     student['grade'] = 'N/A';
+  //   }
+  //   studentsNewGrades.push(student);
+  // }
+
+  return studentsNewGrades;
 }
