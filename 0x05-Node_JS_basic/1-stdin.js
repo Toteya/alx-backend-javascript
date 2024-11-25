1bbfd59176c21baa0da2b9e579dfe2d444c7407b
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-25 20:36:17 +0200

    eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 
-process.stdin.on("readable", () => {
-  let userName = process.stdin.read();
+process.stdin.on('readable', () => {
+  const userName = process.stdin.read();
   if (userName !== null) {
-    process.stdout.write("Your name is: " + userName);
+    process.stdout.write(`Your name is: ${userName}`);
   }
 });
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
