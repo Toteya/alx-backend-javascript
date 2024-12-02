89dd11e77e09438ebb7699d0b5bb9af5838aa928
Toteya <111172685+Toteya@users.noreply.github.com>
2024-12-02 20:09:02 +0200

    test multiple

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber test', () => {
+  it('check addition', () => {
+    assert.equal(calculateNumber('SUM', 2.6, 2.1), 5);
+  });
+  it('check subtraction', () => {
+    assert.equal(calculateNumber('SUBTRACT', 2.6, 2.1), 1);
+  });
+  it('check division', () => {
+    assert.equal(calculateNumber('DIVIDE', 3.8, 2.1), 2);
+  });
+  it('check division by zero', () => {
+    assert.equal(calculateNumber('DIVIDE', 3.8, 0), 'Error');
+  });
+  it('check invalid operation type', () => {
+    assert.equal(calculateNumber('NO_TYPE', 3.8, 1), undefined);
+  });
+});
