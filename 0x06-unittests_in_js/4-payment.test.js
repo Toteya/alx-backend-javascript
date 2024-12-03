f77e8bdc240081c7a30c5db86ff56fb1fbbc7529
Toteya <111172685+Toteya@users.noreply.github.com>
2024-12-03 11:34:57 +0200

    use stubs to simulate behaviour

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should log result to console', () => {
+    const consoleSpy = sinon.spy(console, 'log');
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+    
+    const fake_result = 10;
+    calculateNumberStub.returns(fake_result);
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberStub.calledOnceWith('SUM', 100, 20)).to.be.true;
+    expect(consoleSpy.calledOnceWith(`The total is: ${fake_result}`));
+    calculateNumberStub.restore();
+    consoleSpy.restore();
+  })
+})
