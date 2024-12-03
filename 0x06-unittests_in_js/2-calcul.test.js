e460b072f03cfaccb8ebe1141039460cce7f7349
Toteya <111172685+Toteya@users.noreply.github.com>
2024-12-03 08:49:02 +0200

    chai test

---
+++ 0x06-unittests_in_js/2-calcul.test.js
@@ -0,0 +1,22 @@
+// const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('calculateNumber test', () => {
+  it('check addition', () => {
+    expect(calculateNumber('SUM', 2.6, 2.1)).to.equal(5);
+  });
+  it('check subtraction', () => {
+    expect(calculateNumber('SUBTRACT', 2.6, 2.1)).to.equal(1);
+  });
+  it('check division', () => {
+    expect(calculateNumber('DIVIDE', 3.8, 2.1)).to.equal(2);
+  });
+  it('check division by zero', () => {
+    expect(calculateNumber('DIVIDE', 3.8, 0)).to.equal('Error');
+  });
+  it('check invalid operation type', () => {
+    expect(calculateNumber('NO_TYPE', 3.8, 1)).to.equal(null);
+  });
+});
