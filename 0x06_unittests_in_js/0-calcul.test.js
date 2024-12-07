202800523a2f4c0d4c04f4cb51153f9b2999e4ab
Toteya <111172685+Toteya@users.noreply.github.com>
2024-12-07 10:19:54 +0200

    remove deleted files

--- 0x06_unittests_in_js/0-calcul.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul.js')
-
-describe('calcul test', () => {
-  it('checks equality', () => {
-    assert.equal(calculateNumber(2.3, 2.1), 4);
-  });
-});
